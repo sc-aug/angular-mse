51400493681470b5c10a85e97588b5c9cdc0a779
sc-aug <shichuanwang@yahoo.com>
2018-05-06 14:03:30 -0500

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { NavTopComponent } from './nav-top/nav-top.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { SearchPageComponent } from './search-page/search-page.component';
+import { FavPageComponent } from './fav-page/fav-page.component';
+import { AboutPageComponent } from './about-page/about-page.component';
 
+const appRoutes: Routes = [
+  {path: '', component: HomePageComponent },
+  {path: 'search', component: SearchPageComponent },
+  {path: 'fav', component: FavPageComponent },
+  {path: 'about', component: AboutPageComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavTopComponent,
+    HomePageComponent,
+    SearchPageComponent,
+    FavPageComponent,
+    AboutPageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/fav-page/fav-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-fav-page',
+  templateUrl: './fav-page.component.html',
+  styleUrls: ['./fav-page.component.css']
+})
+export class FavPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
