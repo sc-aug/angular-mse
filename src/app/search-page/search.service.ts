9d316666a001649b5911b0f7655f14112fcced5c
sc-aug <shichuanwang@yahoo.com>
2018-05-10 21:51:29 -0500

    upd service

--- src/app/search-page/search.service.ts
+++ src/app/search-page/search.service.ts
@@ -1,24 +1,23 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-// import { Observable } from 'rxjs/Observable';
 import { catchError, map, tap } from 'rxjs/operators';
+import { of } from 'rxjs/observable/of';
 
+import { environment } from '../../environments/environment';
 import { Music } from '../music-page/music.interface';
 
 @Injectable()
 export class SearchService {
-    baseurl:string = 'https://itunes.apple.com/search';
+  baseurl:string = environment["itunes-api-url"] +'/search';
 
-    constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {}
 
-    dummyCall(term: string) {
-        let tmp = `${this.baseurl}?term=${term}&media=music&limit=20`;
-        return this.http.get<Music[]>(tmp, {responseType: 'json'})
-        .pipe(
-            tap( // Log the result or error
-                // data => console.log(data),
-                error => console.log(error))
-        );
-    }
+  dummyCall(term: string) {
+    let tmp = `${this.baseurl}?term=${term}&media=music&limit=20`;
+    return this.http.get<Music[]>(tmp, {responseType: 'json'})
+    .pipe(
+      catchError(error => of(`Bad Promise: ${error}`))
+    );
+  }
 
 }
